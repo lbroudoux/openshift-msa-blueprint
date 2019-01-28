8f3a5b50964b808f410146d6d588a6d3ce5845bb
lbroudoux <laurent.broudoux@gmail.com>
2019-01-28 17:59:01 +0100

    Initial import of SB service

---
+++ microservice-b/src/main/java/io/openshift/booster/service/GreetingController.java
@@ -0,0 +1,50 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.openshift.booster.service;
+
+import java.util.Objects;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.micrometer.core.annotation.Timed;
+
+@RestController
+public class GreetingController {
+
+    private GreetingProperties properties;
+
+    @Autowired
+    public GreetingController(GreetingProperties properties) {
+        this.properties = properties;
+    }
+
+    @RequestMapping("/api/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        Objects.requireNonNull(properties.getMessage(), "Greeting message was not set in the properties");
+
+        String message = buildGreetingMessage(name);
+        return new Greeting(message);
+    }
+
+    @Timed(value = "greeting.build.message", description = "A timer for internal buildGreetingMessage function", extraTags = "internal")
+    public String buildGreetingMessage(String name) {
+        return String.format(properties.getMessage(), name);
+    }
+}
